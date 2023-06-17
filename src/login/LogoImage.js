9d9301fbf1efa289743d90056b77cbc728691a7c
Erika Avilés <eriavicor34@gmail.com>
2023-06-17 10:43:06 -0500

    Inicio, primeras modificaciones [personalizaciones]

---
+++ src/login/LogoImage.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useMediaQuery } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import { makeStyles } from '@mui/styles';
+import { ReactComponent as Logo } from '../resources/images/logo.svg';
+import { ReactComponent as LogoB } from '../resources/images/logoB.svg';
+
+const useStyles = makeStyles((theme) => ({
+  image: {
+    alignSelf: 'center',
+    maxWidth: '240px',
+    maxHeight: '120px',
+    width: 'auto',
+    height: 'auto',
+    [theme.breakpoints.down('lg')]: {
+      width: '1440px',
+    },
+    [theme.breakpoints.down('sm')]: {
+      width: '320px',
+    },
+  },
+}));
+
+const LogoImage = () => {
+  const classes = useStyles();
+  const theme = useTheme();
+  return (
+    !useMediaQuery(theme.breakpoints.down('lg'))
+      ? <LogoB className={classes.image} alt="Logo de la marca dailyGPS" />
+      : <Logo className={classes.image} alt="Logo de la marca dailyGPS" />
+  );
+};
+
+export default LogoImage;
